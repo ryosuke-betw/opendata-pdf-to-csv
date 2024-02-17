1468fbe55f293a3a5ad87ae461b903cc20f7e0cf
TokioMiyaoka <miyaoka.tokio@tis.co.jp>
2024-02-17 16:40:57 +0900

    integrate csv files by order

--- action.py
+++ action.py
@@ -1,13 +1,14 @@
 import pandas as pd
 import glob
+import os
 
 
 def integrate(input_file_path, output_file_name):
-    csv_files = glob.glob(f"{input_file_path}/*.csv")
+    number = sum(os.path.isfile(os.path.join(input_file_path, name)) for name in os.listdir(input_file_path))
     data_list = []
 
-    for csv_file in csv_files:
-        data_list.append(pd.read_csv(csv_file))
+    for i in range(0, number):
+        data_list.append(pd.read_csv(f"{input_file_path}/{i+1}.csv"))
 
     df = pd.concat(data_list, axis=0, sort=True)
 
