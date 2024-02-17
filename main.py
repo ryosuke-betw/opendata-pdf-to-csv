462e295e4b58b5d5ba736d92f8a28394f5bd2b02
TokioMiyaoka <miyaoka.tokio@tis.co.jp>
2024-02-17 14:37:30 +0900

    delete temporary csv files after integrating

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import tabula
 from action import integrate
+import os
+import shutil
 
 # lattice=Trueでテーブルの軸線でセルを判定
 dfs = tabula.read_pdf("000876235.pdf", lattice=True, pages='all', pandas_options={'header': None})
@@ -10,4 +12,8 @@ for df in dfs:
     df = df.replace('\n', '', regex=True).replace('\r', '', regex=True).replace('\r\n', '', regex=True).replace('\n\r', '', regex=True)
     print(df)
     df.to_csv(f"./files/{i}.csv", index=None)
+
 integrate("./files", "output_files")
+
+shutil.rmtree("./files")
+os.mkdir("./files")
