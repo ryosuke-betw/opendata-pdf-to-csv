98ce7bb00457ee2bed38c1ea5b4a2f17cbb8e06f
ryo-ma <saka_ro@yahoo.co.jp>
2024-02-18 12:02:53 +0900

    fix(main.py): fix off-by-one error in the range of the for loop to correctly iterate over prefecture numbers

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def delete_headers(df, line_number):
 if not os.path.exists("./output_files"):
     os.mkdir("./output_files")
 
-for i in range(1, 47):
+for i in range(1, 48):
     print("PREFECTURE_NUMBER", i, PREFECTURES[i-1])
     opendata_file = os.listdir(f"./data_files/shinryoujo_{i}")
     dfs = tabula.read_pdf(f"./data_files/shinryoujo_{i}/{opendata_file[0]}", lattice=True, pages='all', pandas_options={'header': None})
