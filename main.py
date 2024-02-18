d875338f763a05ca099f6043c31e4771c06f6b42
ryo-ma <saka_ro@yahoo.co.jp>
2024-02-18 16:07:56 +0900

    fix(main.py): refactor delete_title function to remove unnecessary title specific to Daihuku Prefecture fix(main.py): refactor fix_format_page_df function to apply delete_headers and delete_title functions to the dataframe fix(main.py): refactor merged_df variable to df for clarity fix(main.py): refactor result_df variable to remove unnecessary columns with missing values and rows with less than 2 data points fix(main.py): refactor output file name to include prefecture number and name

--- main.py
+++ main.py
@@ -10,6 +10,17 @@ def delete_headers(df, line_number):
     return df
 
 
+# 大分県に不要なタイトルがあるため削除
+def delete_title(df):
+    if df.iloc[0, 0] == "緊急避妊に係る診療が可能な産婦人科医療機関等一覧":
+        return df.drop(df.index[:1])
+    return df
+
+
+def fix_format_page_df(df, line_number):
+    return delete_headers(delete_title(df), line_number)
+
+
 if not os.path.exists("./output_files"):
     os.mkdir("./output_files")
 
@@ -18,12 +29,16 @@ for i in range(1, 48):
     opendata_file = os.listdir(f"./data_files/shinryoujo_{i}")
     dfs = tabula.read_pdf(f"./data_files/shinryoujo_{i}/{opendata_file[0]}", lattice=True, pages='all', pandas_options={'header': None})
     # 1ページ目のみ「基本情報」行の削除のため1行指定
-    first_df = delete_headers(dfs[0], 1)
+    first_df = fix_format_page_df(dfs[0], 1)
     # 2ページ目以降は「基本情報」およびヘッダーを削除するため2行指定
-    dfs = [delete_headers(x, 2) for x in dfs[1:]]
+    dfs = [fix_format_page_df(x, 2) for x in dfs[1:]]
     dfs.insert(0, first_df)
-    merged_df = pd.concat(dfs).replace('\n', '', regex=True).replace('\r', '', regex=True).replace('\r\n', '', regex=True).replace('\n\r', '', regex=True)
-    # データが2つ未満の行は不要な可能性が高いので削除
-    result_df = merged_df.dropna(thresh=2)
+    # ページごとのデータを結合
+    df = pd.concat(dfs)
+    # 改行コードを削除
+    df = df.replace('\n', '', regex=True).replace('\r', '', regex=True).replace('\r\n', '', regex=True).replace('\n\r', '', regex=True)
+    # データが2つ未満の行は不要な可能性が高いので行を削除 & 列名に欠損値がある場合も列ごと削除
+    result_df = df.dropna(thresh=2).dropna(subset=[df.index[0]], axis=1)
+
     prefecture_number = str(i).zfill(2)
     result_df.to_csv(f"./output_files/{prefecture_number}_{PREFECTURES[i-1]}.csv", header=False, index=False)
